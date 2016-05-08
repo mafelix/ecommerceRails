1a8e2b19cd6ed501c5523c06220e6c26520c2f0a
mafelix <felixmdev@gmail.com>
2016-05-08 22:06:22 +0000

    search is added to navbar instead, class styling added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,21 @@
 class ProductsController<ApplicationController
 
   def index
-    if params[:search] == "" && params[:category] == "Select Category"
+    if params[:search] == "" || params[:search].nil?
       @products = Product.all
 
-    elsif params[:search] != ""
+    # end
+    elsif params[:search] != "" && params[:search] != nil
       params[:search] = params[:search].downcase
       @products = Product.searchName(params[:search])
+    # elsif params[:category] == "Green"
+    #   @products = Product.green
+    # elsif params[:category] == "Accessories"
+    #   @prodcuts = Product.Accessories
+    # elsif params[:category] == "Herbal"
+    #   @products = Product.herbal 
     end
+
   end
 
   def show
