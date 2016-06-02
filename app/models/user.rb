152f037b0bb56ba9728746fea54887165ab6fd27
mafelix <felixmdev@gmail.com>
2016-06-02 21:49:58 +0000

    canadian postal_code validator added

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,15 +18,13 @@ class User < ActiveRecord::Base
 
 
   canadian_postal_code = /\A[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1}[ -]?\\d{1}[A-Z]{1}\\d{1}\z/
-  valid_email = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # valid_email = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates :address, presence: true
-  # validates :postal_code, presence: true
-  # ,format: {with: canadian_postal_code,
-  #                                  message: "Only canadian postal codes"}
-  validates :email, presence: true, format: {with: valid_email,
-                                             message: "Invalid email address" }
+  validates :postal_code, presence: true ,format: {with: canadian_postal_code}
+  # validates :email, presence: true, format: {with: valid_email,
+                                             # message: "Invalid email address" }
   # validates :password_digest, presence: true
 
 
