f9a1b244aa8365f3e34bd6c283f12b14e724b351
mafelix <felixmdev@gmail.com>
2016-06-06 07:46:44 +0000

    path in aws changed for image uploads ;

--- app/uploaders/pictureid_uploader.rb
+++ app/uploaders/pictureid_uploader.rb
@@ -13,7 +13,7 @@ class PictureidUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{model.id}/#{model.first_name.capitalize}-#{model.last_name.capitalize}"
+    "/#{model.class.to_s.underscore}/#{model.id}/#{model.first_name.capitalize}-#{model.last_name.capitalize}/#{mounted_as}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
