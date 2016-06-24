aab333f5e2bee163498affd0b30ab644bdd2bb7e
mafelix <felixmdev@gmail.com>
2016-06-24 22:52:33 +0000

    more media queries for certain pages

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,19 @@
 class Order<ActiveRecord::Base
 # still need to modify database schema to reflect associations
   belongs_to :user
-  has_many :carts
-  before_save :update_subtotal
+  # has_many :carts
+  # before_save :update_subtotal
 
   # before saving the order update the subtotal of the order.
 
-  def subtotal
-    cart_items.collect {|cart_items| cart_items.valid? ? (cart_items.quantity * cart_items.unit_price) : 0}.sum
-  end
-
-  private
-
-  def update_subtotal
-      self[:subtotal] = subtotal
-  end
+  # def subtotal
+  #   cart_items.collect {|cart_items| cart_items.valid? ? (cart_items.quantity * cart_items.unit_price) : 0}.sum
+  # end
+  #
+  # private
+  #
+  # def update_subtotal
+  #     self[:subtotal] = subtotal
+  # end
 
 end
